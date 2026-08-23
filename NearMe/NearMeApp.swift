a92b0b9a35276ba085409b03686e0967ef1733c4
Udhayakumar Velou <udhayakumar2352001@gmail.com>
2026-08-23 12:32:09 +0200

    Initial Commit

---
+++ NearMe/NearMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeApp.swift
+//  NearMe
+//
+//  Created by Udhaya on 23/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct NearMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
